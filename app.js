79b6c05d485a9efc4cc2f588d8e68e5c54ef812b
Kgiberson <kari.giberson@gmail.com>
2016-07-22 10:52:47 -0700

    Add link input field to form

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ app.controller('MainCtrl', [
     // When we add a post we get the title from $scope.title, which is then cleared after the post has been created.
     $scope.addPost = function(){
       if (!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
       $scope.title = '';
+      $scope.link = '';
     };
     @scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
